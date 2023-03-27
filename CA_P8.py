fd37c0b7b6ec5945fc41aae8d74bc8e1a7ea3984
aniketbhagat16 <126847451+aniketbhagat16@users.noreply.github.com>
2023-03-27 17:04:44 +0000

    main

--- CA_P8.py
+++ CA_P8.py
@@ -17,7 +17,7 @@ print(num_unique, '\n')"""
 
 sentence= "Hey I am walking here I am walking here o captain my captain water water everywhere nor a drop to drink"
 sl=sentence.split()
-print("reverse= "," ".join(sl[::-1]))
+print("Verse= "," ".join(sl[:15]))
 print("sentence_list= ",sl,"\n")
 
 ss=set(sl)
